f3cc50b66657a69058ec77b0a35c9380a96dc833
Omar Moreno <ormoreno@unipanamericana.edu.co>
2017-07-07 13:57:17 -0500

    Added project app

--- ViewModels/General/UserActivationResponse.cs
+++ ViewModels/General/UserActivationResponse.cs
@@ -10,7 +10,7 @@ namespace ViewModels.General
     {
         public string UserCellPhone { get; set; }
         public string UserPassword { get; set; }
-        public string SaleDate { get; set; }
-        public string ValidUntil { get; set; }
+        public DateTime SaleDate { get; set; }
+        public DateTime ValidUntil { get; set; }
     }
 }
